29fa1fb00df1262f328cf4d2fba14cc8d253f340
marianne <mariannepeterson92@gmail.com>
2026-05-30 18:39:40 +0200

    ambtenaar matches bekijken

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,6 +9,8 @@ const routes = [
   { path: '/voortgang', name: 'voortgang', component: () => import('../views/ProgressView.vue') },
   { path: '/ambtenaar', name: 'ambtenaar', component: () => import('../views/AmbtenaarView.vue') },
   { path: '/ambtenaar/:id', name: 'ambtenaar-case', component: () => import('../views/AmbtenaarCaseView.vue') },
+  { path: '/ambtenaar/:pandId/aanvrager/:personId', name: 'ambtenaar-aanvrager', component: () => import('../views/AmbtenaarWoningzoekendeView.vue') },
+  { path: '/ambtenaar/:pandId/vervolgstappen', name: 'ambtenaar-vervolgstappen', component: () => import('../views/VervolgstappenView.vue') },
 ];
 
 export const router = createRouter({
